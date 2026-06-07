c98cf3ba1e198df343c7cd1430e67aa09b97aa09
Silentx47 <ezra.jp.422@gmail.com>
2026-06-07 16:26:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def login():
             flash('Invalid username or password')
 
     return render_template('login.html')
-
 @app.route('/dashboard')
 def dashboard():
     if 'username' not in session:
@@ -116,27 +115,48 @@ def dashboard():
             top_item_count = 0
     cur.execute("""
     SELECT payment_method,
-           COUNT(*) AS count
+           COUNT(*) AS total
 
     FROM sales
 
     WHERE payment_method IS NOT NULL
 
     GROUP BY payment_method
+""")
+    payment_data = cur.fetchall()
+    payments = {
+    'UPI': 0,
+    'Cash': 0,
+    'Card': 0 }
 
-    ORDER BY count DESC
+    for method, count in payment_data:
+        payments[method] = count
+    max_payment = max(payments.values())
+    if max_payment == 0:
+        max_payment = 1
+
+   
+    cur.execute("""
+    SELECT DAYNAME(created_at),
+           SUM(total) AS revenue
+
+    FROM sales
+
+    GROUP BY DAYNAME(created_at)
+
+    ORDER BY revenue DESC
 
     LIMIT 1
 """)
 
-    payment = cur.fetchone()
+    result = cur.fetchone()
 
-    if payment:
-        favorite_payment = payment[0]
-        payment_count = payment[1]
+    if result:
+        best_day = result[0]
+        best_day_revenue = result[1]
     else:
-        favorite_payment = "N/A"
-        payment_count = 0
+        best_day = "N/A"
+        best_day_revenue = 0
     cur.execute("""
     SELECT DAYNAME(created_at),
            SUM(total)
@@ -159,7 +179,130 @@ def dashboard():
 
     for day, revenue in weekly_sales:
      week_data[day] = float(revenue)
-    
+    max_revenue = max(week_data.values())
+
+    if max_revenue == 0:
+        max_revenue = 1
+    cur.execute("""
+    SELECT HOUR(created_at),
+           COUNT(*) AS orders_count
+
+    FROM sales
+
+    GROUP BY HOUR(created_at)
+
+    ORDER BY orders_count DESC
+
+    LIMIT 1
+""")
+
+    peak = cur.fetchone()
+
+    if peak:
+        start_hour = peak[0]
+        end_hour = (start_hour + 1) % 24
+
+        peak_hour = f"{start_hour:02d}:00 - {end_hour:02d}:00"
+        peak_orders = peak[1]
+    else:
+        peak_hour = "N/A"
+        peak_orders = 0
+    cur.execute("""
+    SELECT MONTH(created_at),
+           SUM(total)
+
+    FROM sales
+
+    GROUP BY MONTH(created_at)
+
+    ORDER BY MONTH(created_at) DESC
+
+    LIMIT 2
+""")
+
+    months = cur.fetchall()
+
+    if len(months) == 2:
+
+        current = float(months[0][1])
+        previous = float(months[1][1])
+
+        if previous > 0:
+            growth = ((current - previous) / previous) * 100
+        else:
+            growth = 100
+
+    else:
+        growth = 0
+    cur.execute("""
+    SELECT IFNULL(SUM(total), 0)
+
+    FROM sales
+
+    WHERE DATE(created_at) = CURDATE()
+""")
+
+    today_revenue = cur.fetchone()[0]
+    cur.execute("""
+    SELECT customer_name,
+           total,
+           status
+
+    FROM orders
+
+    ORDER BY id DESC
+
+    LIMIT 5
+""")
+
+    recent_orders = cur.fetchall()
+    cur.execute("""
+    SELECT product_name,
+           SUM(quantity) AS sold
+
+    FROM sales
+
+    WHERE product_name IS NOT NULL
+
+    GROUP BY product_name
+
+    ORDER BY sold DESC
+
+    LIMIT 5
+""")
+
+    top_items = cur.fetchall()
+    week_values = list(week_data.values())
+    top_labels = [item[0] for item in top_items]
+    top_counts = [item[1] for item in top_items]
+    cur.execute("""
+    SELECT name,
+           role,
+           status
+
+    FROM staff
+""")
+
+    staff_list = cur.fetchall()
+    cur.execute("SELECT COUNT(*) FROM staff")
+    total_staff = cur.fetchone()[0]
+
+    cur.execute("SELECT COUNT(*) FROM staff WHERE status='On Duty'")
+    on_duty = cur.fetchone()[0]
+
+    cur.execute("SELECT COUNT(*) FROM staff WHERE status='Absent'")
+    absent = cur.fetchone()[0]
+
+    cur.execute("SELECT COUNT(*) FROM staff WHERE status='On Leave'")
+    on_leave = cur.fetchone()[0]
+    cur.execute("""
+    SELECT id,
+           product_name,
+           price
+    FROM products
+""")
+
+    products = cur.fetchall()
     cur.close()
     return render_template(
         'dashboard.html',
@@ -178,10 +321,25 @@ def dashboard():
         payment=payment,
         top_item_name=top_item_name,
         top_item_count=top_item_count,
-        favorite_payment=favorite_payment,
-        payment_count=payment_count,
-        weekly_sales=weekly_sales
-        )
+        best_day_revenue=best_day_revenue,
+        payments=payments,
+        max_payment=max_payment,
+        best_day=best_day,  
+        week_data=week_data,
+        max_revenue=max_revenue,
+        peak_hour=peak_hour,
+        peak_orders=peak_orders,
+        growth=round(growth, 1),
+        total_orders = active_orders + completed_orders,
+        recent_orders = recent_orders,
+         week_values=week_values,
+        top_labels=top_labels,
+        top_counts=top_counts,
+        staff_list=staff_list,
+        total_staff=total_staff,
+        on_duty=on_duty,
+        absent=absent,
+        on_leave=on_leave, )
 
 @app.route('/add_order', methods=['POST'])
 def add_order():
@@ -203,7 +361,7 @@ def add_order():
         VALUES (%s, %s, %s)
         """,
 
-        (customer_name, total, status)
+        (customer_name, total, "Pending")
     )
 
     mysql.connection.commit()
@@ -279,6 +437,91 @@ def create_bill():
     cur.close()
 
     return redirect(url_for('dashboard'))
+@app.route('/products')
+def products():
+    cursor = mysql.connection.cursor()
+
+    cursor.execute("""
+        SELECT id, product_name, price
+        FROM products
+    """)
+
+    products = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template(
+        'products.html',
+        products=products
+    )
+@app.route('/add_product', methods=['POST'])
+def add_product():
+
+    product_name = request.form['product_name']
+
+    price = request.form['price']
+
+    cursor = mysql.connection.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO products (
+            product_name,
+            price
+        )
+        VALUES (%s, %s)
+        """,
+        (product_name, price)
+    )
+
+    mysql.connection.commit()
+
+    cursor.close()
+
+    return redirect(url_for('dashboard'))
+@app.route('/edit_product/<int:id>', methods=['POST'])
+def edit_product(id):
+
+    product_name = request.form['product_name']
+    price = request.form['price']
+    print("EDIT ROUTE HIT")
+    print(request.form)
+    cursor = mysql.connection.cursor()
+
+    cursor.execute(
+        """
+        UPDATE products
+        SET product_name = %s,
+            price = %s
+        WHERE id = %s
+        """,
+        (product_name, price, id)
+    )
+
+    mysql.connection.commit()
+
+    cursor.close()
+
+    return redirect(url_for('dashboard'))
+
+@app.route('/delete_product/<int:id>', methods=['POST'])
+def delete_product(id):
+    print(f"Deleting product ID: {id}")
+    cursor = mysql.connection.cursor()
+
+    cursor.execute(
+        """
+        DELETE FROM products
+        WHERE id = %s
+        """,
+        (id,)
+    )
+
+    mysql.connection.commit()
+
+    cursor.close()
+
+    return redirect(url_for('dashboard')) 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
